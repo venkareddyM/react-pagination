00ed2fb090a264388d155f1de1cc2f1420a305f8
mukkarlareddy <venkareddymukkarla@gmail.com>
2023-07-25 06:53:08 +0530

    'firstcommit'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [searchText, setSearchText] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/comments');
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+    setCurrentPage(1); // Reset page number when performing a new search
+  };
+
+  const filteredData = data.filter((comment) => {
+    const searchLower = searchText.toLowerCase();
+    return (
+      comment.name.toLowerCase().includes(searchLower) ||
+      comment.email.toLowerCase().includes(searchLower) ||
+      comment.body.toLowerCase().includes(searchLower)
+    );
+  });
+
+  // Calculate the indexes of the data to be displayed on the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentData = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Function to handle page changes
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  // Calculate the total number of pages
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Comments</h1>
+      <input
+        type="text"
+        value={searchText}
+        onChange={handleSearchChange}
+        placeholder="Search..."
+      />
+      <table border={2}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentData.map((comment) => (
+            <tr key={comment.id}>
+              <td>{comment.id}</td>
+              <td>{comment.name}</td>
+              <td>{comment.email}</td>
+              <td>{comment.body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      {/* Pagination Controls */}
+      <div style={{ marginTop: '20px' }}>
+        {Array.from({ length: totalPages }, (_, index) => (
+          <button key={index} onClick={() => handlePageChange(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
